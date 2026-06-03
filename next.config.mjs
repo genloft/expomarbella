ecc04b1b09c6844b239d721efef0374b7ec1b616
pro0 <garciagarcia.juanantonio@gmail.com>
2026-06-03 15:40:01 +0200

    fix: allow external image domains for Next.js Image component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'decomarbella.es',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.calameo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'p.calameoassets.com',
+      }
+    ],
+  },
+};
 
 export default nextConfig;
